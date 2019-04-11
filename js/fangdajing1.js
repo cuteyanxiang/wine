12d58fe9f6e678c8d7561bb09e46eb74a46b2774
lyc <1960249529@qq.com>
2019-04-11 16:43:20 +0800

    等待登录页

--- js/fangdajing1.js
+++ js/fangdajing1.js
@@ -3,14 +3,14 @@ function Zoom() {
     return document.getElementById(id);
   }
   this.Detail = $id("detail");
-  //console.log(this.Detail);
+
   this.imgZoom = $id("imgZoom");
   this.yidong = $id("yidong")
   this.imgImg = this.imgZoom.children[0];
-  //console.log(this.imgImg);
+
   this.fdqk = $id("fdqk");
   this.bigImg = this.fdqk.children[0];
-  //console.log(this.bigImg);
+
   this.imgZoom.onmouseover = () => {
     this.yidong.style.display = "block";
     this.fdqk.style.display = "block";
@@ -21,13 +21,12 @@ function Zoom() {
   }
   this.imgZoom.onmousemove = (e) => {
     var evt = e || event;
-    // var y = evt.offsetY - this.yidong.offsetHeight / 2;
-    var x = evt.pageX - this.fdqk.offsetLeft - this.yidong.offsetWidth / 2;
-    var y = evt.pageY - this.fdqk.offsetTop - this.yidong.offsetHeight / 2;
-    //console.log(x,y);
-    var maxt = this.imgZoom.offsetHeight - this.yidong.offsetHeight;
-    var maxl = this.imgZoom.offsetWidth - this.yidong.offsetWidth;
-    console.log(maxt, maxl);
+    var y = evt.pageY -  this.yidong.offsetHeight/2-this.imgZoom.offsetTop-this.Detail.offsetTop;
+    var x = evt.pageX - this.yidong.offsetWidth/2-this.imgZoom.offsetLeft-this.Detail.offsetLeft;
+
+    var maxt = this.imgZoom.offsetHeight- this.yidong.offsetHeight;
+    var maxl = this.imgZoom.offsetWidth-this.yidong.offsetWidth;
+
     x = x <= 0 ? 0 : x >= maxl ? maxl : x;
     y = y <= 0 ? 0 : y >= maxt ? maxt : y;
 
@@ -35,8 +34,8 @@ function Zoom() {
     this.yidong.style.top = y + "px";
 
 
-    this.bigImg.style.left = -x / this.imgZoom.offsetWidth * this.bigImg.offsetWidth + "px";
-    this.bigImg.style.top = -y / this.imgZoom.offsetHeight * this.bigImg.offsetHeight + "px";
+    this.bigImg.style.left = -2*x  + "px";
+    this.bigImg.style.top = -2*y  + "px";
 
   }
 }
